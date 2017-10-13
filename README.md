cdb976d0ee9bbad35b0eb3d36fe0182e486eed7a
Andreas Moser <amoser@google.com>
2017-10-13 12:20:18 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# yara-procdump-python
+A Python extension to wrap the Yara process memory access API.
+
+This is not an official Google product.
---
+++ setup.py
@@ -0,0 +1,34 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Setup.py for yara-procdump-python."""
+
+import setuptools
+
+_yara_procdump = setuptools.Extension('yara_procdump',
+                                      sources=['yara_procdump_python.c'],
+                                      libraries=['yara'])
+
+description = 'A Python extension to wrap the Yara process memory access API.'
+
+setuptools.setup(
+    name='yara-procdump-python',
+    version='0.1',
+    description=description,
+    long_description=open('README.md').read(),
+    license='Apache 2.0',
+    author='Andreas Moser',
+    author_email='amoser@google.com',
+    url='https://github.com/google/yara-procdump-python',
+    ext_modules=[_yara_procdump])
