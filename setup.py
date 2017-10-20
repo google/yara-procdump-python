54d1cf789a644ed1d138d82cb25498ad16186f13
Andreas Moser <amoser@google.com>
2017-10-20 15:21:54 +0200

    Make static linking the default, dynamic linking is optional.

--- setup.py
+++ setup.py
@@ -14,11 +14,22 @@
 
 """Setup.py for yara-procdump-python."""
 
+from distutils.command import build
+from distutils.command import build_ext
 import setuptools
 
 _yara_procdump = setuptools.Extension('yara_procdump',
                                       sources=['yara_procdump_python.c'],
+                                      include_dirs=['yara/libyara/include'],
                                       libraries=['yara'])
+
+OPTIONS = [
+    ('dynamic-linking', None, 'link dynamically against libyara'),
+]
+
+BOOLEAN_OPTIONS = [
+    'dynamic-linking',
+]
 
 description = 'A Python extension to wrap the Yara process memory access API.'
 
@@ -28,13 +39,56 @@ except IOError:
   long_description = description
 
 
+class BuildCommand(build.build):
+
+  user_options = build.build.user_options + OPTIONS
+  boolean_options = build.build.boolean_options + BOOLEAN_OPTIONS
+
+  def initialize_options(self):
+    build.build.initialize_options(self)
+    self.dynamic_linking = None
+
+
+class BuildExtCommand(build_ext.build_ext):
+  """A custom command to build the extension."""
+
+  user_options = build_ext.build_ext.user_options + OPTIONS
+  boolean_options = build_ext.build_ext.boolean_options + BOOLEAN_OPTIONS
+
+  def initialize_options(self):
+    build_ext.build_ext.initialize_options(self)
+    self.dynamic_linking = None
+
+  def finalize_options(self):
+    build_ext.build_ext.finalize_options(self)
+    self.set_undefined_options('build',
+                               ('dynamic_linking', 'dynamic_linking'))
+
+  def run(self):
+    """Execute the build command."""
+
+    module = self.distribution.ext_modules[0]
+
+    if self.dynamic_linking:
+      module.libraries.append('yara')
+    else:
+      module.sources.append('yara/libyara/proc.c')
+      module.sources.append('yara/libyara/mem.c')
+
+    build_ext.build_ext.run(self)
+
+
 setuptools.setup(
     name='yara-procdump-python',
-    version='0.1.0.post1',
+    version='0.1.0.post2',
     description=description,
     long_description=long_description,
     license='Apache 2.0',
     author='Andreas Moser',
     author_email='amoser@google.com',
     url='https://github.com/google/yara-procdump-python',
+    cmdclass={
+        'build': BuildCommand,
+        'build_ext': BuildExtCommand,
+    },
     ext_modules=[_yara_procdump])
